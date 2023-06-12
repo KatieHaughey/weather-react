f2f656a055583302b21abbbf8f7777102a2b735a
KatieHaughey <132608324+KatieHaughey@users.noreply.github.com>
2023-06-12 12:31:46 +0100

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
-      <br />
-      <div id="open-source">
-        <a
-          href="https://github.com/KatieHaughey/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code{" "}
-        </a>
-        by Katie Haughey
+      <div className="container">
+        <Weather />
+        <br />
+        <div id="open-source">
+          <a
+            href="https://github.com/KatieHaughey/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code{" "}
+          </a>
+          by Katie Haughey
+        </div>
       </div>
     </div>
   );
